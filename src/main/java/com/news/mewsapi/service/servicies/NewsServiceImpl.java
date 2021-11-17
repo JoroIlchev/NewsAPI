37f8f24a84177d17372b701a5fda3ef9653c50a4
GeorgiIlchev <georgi.ilchev@yahoo.com>
2021-11-17 11:35:58 +0200

    Trying conflict

--- src/main/java/com/news/mewsapi/service/servicies/NewsServiceImpl.java
+++ src/main/java/com/news/mewsapi/service/servicies/NewsServiceImpl.java
@@ -17,7 +17,7 @@ public class NewsServiceImpl implements NewsService {
     private final NewsRepository newsRepository;
     private final ModelMapper modelMapper;
 
-    
+    //Some other test here
     public NewsServiceImpl(NewsRepository newsRepository, ModelMapper modelMapper) {
         this.newsRepository = newsRepository;
         this.modelMapper = modelMapper;
